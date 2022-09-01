1e639c24bb53f2dd6785a255a786cbe1f7e67cac
prabha666 <108609740+prabha666@users.noreply.github.com>
2022-09-01 23:31:00 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,84 @@
+// import React, { Component, useState } from "react";
+// import "../styles/App.css";
+
+// class App extends Component {
+//   render() {
+//     let relatives = [
+//       "Nazir",
+//       "Sabir",
+//       "Imran",
+//       "Prahlad",
+//       "Shiwam",
+//       "Vikash"
+//     ];
+//     return (
+//       <div id="main">
+//         <ol key="relativeList">
+//           {relatives.map((relative, index) => (
+//             <li key={`relativeListItem${index + 1}`}>{relative}</li>
+//           ))}
+//         </ol>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
+
+
+
+
+
+
+
 import React, {Component, useState} from "react";
 import '../styles/App.css';
 
-class App extends Component {
-    render() {
-
-        return(
-            <div id="main">
-               {/* Do not remove the main div */}
-            </div>
-        )
+const App =()=> {
+  var arr  = ["Siblings","Friends","Love","Affection","Marriage","Enemy","Please Enter valid input" ];
+  const [getrel ,setRel ] = useState();
+  
+  const calRelation = () => {
+    let input1 = document.querySelector('#inputfirst').value;
+  let input2 = document.querySelector('#inputlast').value;
+    if (input1 === "" || input2 === "") {
+      setRel(6);
+    } else {    
+     input1 = input1.toLowerCase();
+     input2 = input2.toLowerCase();
+      function match(input1, input2) {
+        let count = 0;
+        for (let i in input1){
+          if(input2.includes(input1[i])){
+          ++count;
+          }
+        }
+        return count;
+      }
+      const comm = match(input1,input2);
+      let l1 = Number(input1.length);
+      let l2 = Number(input2.length);
+      let len = Number(comm);
+       let n  = l1+l2-len*2;
+       setRel(n%6);
     }
+  }
+  const clearRelation = () => {
+     document.querySelector('#inputfirst').value="";
+     document.querySelector('#inputlast').value="";
+     setRel("");
+
+  }
+  return (
+    <div id="main">
+      <label>First Name</label><input type="text" data-testid="input1" id="inputfirst"></input> <br></br>
+      <label>Last Name</label><input type="text" data-testid="input2" id="inputlast"></input><br></br>
+      <button data-testid="calculate_relationship" onClick={calRelation} >Calculate Relationship Future</button>
+      <button data-testid="clear" onClick={clearRelation}>Clear inputs and relationship status</button>
+      <h3 data-testid="answer" id="relation">{arr[getrel]}</h3>
+    </div>
+  )
 }
 
 
